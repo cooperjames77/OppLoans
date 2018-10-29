adb3fdb893b73c88a6faa43c99b8ba02b5b6cc87
coopjame <coopjame@umich.edu>
2018-10-29 10:40:06 -0400

    first commit

---
+++ Test.py
@@ -0,0 +1,12 @@
+x = "Hello, World!"
+print(x)
+print(x + " OMG")
+numbers = [1, 2, 3, 4, 5]
+for x in range(1, 3):
+    print(numbers[x])
+y = 4
+z = 2.3
+if z > y:
+    print("yes")
+elif y > z:
+    print("no")
